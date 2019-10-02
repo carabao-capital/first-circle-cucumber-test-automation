48475ade6b53d3dfa70a570f3b063b3cbbf5a4c8
RJ Angeles <rj.angeles@firstcircle.com>
2019-10-02 10:37:00 +0800

    Push cucumber repository to github

---
+++ features/fca_customer_login.feature
@@ -0,0 +1,7 @@
+Feature: FCA customer login
+    Tests customer login on FCA
+
+    Scenario: As a customer I want to login with valid credentials
+        Given Customer loads FCA login page
+        When Customer submits valid login credentials
+        Then Customer should be successfully logged in FCA
---
+++ features/step_definitions/fca_step_definitions/fca_login_page.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :chrome
+Given("Customer loads FCA login page") do
+    driver.get "https://preprod.my.firstcircle.ph/login"
+end
+  
+When("Customer submits valid login credentials") do
+    email_field = driver.find_element(id: "user_login")
+    password_field = driver.find_element(id: "user_password")
+    login_button = driver.find_element(css: "button[type='submit']")
+    email_field.send_keys "no_progress1@firstcircle.com"
+    password_field.send_keys "hello123"
+    login_button.click
+end
+  
+Then("Customer should be successfully logged in FCA") do
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    wait.until { driver.find_element(css: "a.ds-btnafad")}
+    driver.quit
+end
+  
+
